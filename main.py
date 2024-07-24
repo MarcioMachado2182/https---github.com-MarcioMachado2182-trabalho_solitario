60828fac0337e720fdc760e22d2fb10da33e69c7
MarcioMachado2182 <marciojjb@gmail.com>
2024-07-24 16:45:34 -0300

    Trabalho Solo

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # main.py
+
 import tkinter as tk
 from controller.usuario_c import UsuarioController
 
 def main():
     root = tk.Tk()
     app = UsuarioController(root)
+    app.show_usuario_view()  # Exibe a tela inicial ao iniciar o aplicativo
     root.mainloop()
 
 if __name__ == "__main__":
