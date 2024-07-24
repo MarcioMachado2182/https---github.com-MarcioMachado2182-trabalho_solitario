4b895bb7641944f618f4b7e12802464c5e813fdf
MarcioMachado2182 <marciojjb@gmail.com>
2024-07-24 17:21:07 -0300

    ultimoi

--- controller/usuario_c.py
+++ controller/usuario_c.py
@@ -1,46 +1,53 @@
-# controller.py
-from model.usuario_m import UsuarioModel
-from view.login_v import LoginView
-from view.cadastro_v import CadastroView
-from view.usuario_v import UsuarioView
+
+
 
 class UsuarioController:
     def __init__(self, root):
         self.model = UsuarioModel()
         self.root = root
-        self.usuario_view = UsuarioView(root, self)
+        self.usuario_view = None
         self.login_view = None
         self.register_view = None
 
+    def hide_other_views(self, view_to_show):
+        # Oculta a tela de login se estiver aberta
+        if self.login_view and self.login_view.winfo_exists() and self.login_view != view_to_show:
+            self.login_view.withdraw()
+
+        # Oculta a tela de cadastro se estiver aberta
+        if self.register_view and self.register_view.winfo_exists() and self.register_view != view_to_show:
+            self.register_view.withdraw()
+
+        # Oculta a tela do usuário se estiver aberta
+        if self.usuario_view and self.usuario_view.winfo_exists() and self.usuario_view != view_to_show:
+            self.usuario_view.withdraw()
+
     def show_login_view(self):
+        self.hide_other_views(self.login_view)
+
         if self.login_view is None or not self.login_view.winfo_exists():
             self.login_view = LoginView(self.root, self)
         else:
             self.login_view.deiconify()
 
     def show_register_view(self):
+        self.hide_other_views(self.register_view)
+
         if self.register_view is None or not self.register_view.winfo_exists():
             self.register_view = CadastroView(self.root, self)
         else:
             self.register_view.deiconify()
 
-    def login(self):
-        username = self.login_view.get_username()
-        password = self.login_view.get_password()
-        if self.model.authenticate(username, password):
-            print(f"Usuário {username} logado com sucesso!")
-        else:
-            self.login_view.show_error("Nome de usuário ou senha incorretos.")
+    def show_usuario_view(self):
+        self.hide_other_views(self.usuario_view)
 
-    def register(self):
-        username = self.register_view.username_entry.get()
-        password = self.register_view.password_entry.get()
-        if username and password:
-            self.model.add_user(username, password)
-            print(f"Usuário {username} cadastrado com sucesso!")
-            self.register_view.destroy()
+        if self.usuario_view is None or not self.usuario_view.winfo_exists():
+            self.usuario_view = UsuarioView(self.root, self)
         else:
-            self.register_view.show_error("Por favor, preencha todos os campos.")
+            self.usuario_view.deiconify()
+
+    # Restante do seu código...
+
 
 
     
